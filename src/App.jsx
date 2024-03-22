e0e00c0fa9b3cad5cc94af02b299093c60df9767
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-03-22 11:23:48 -0400

    all relevant data for omb

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-import dummyAPI from "./dummyAPI";
+import ombAPI from "./ombAPI";
 
 function App() {
   return (
     <>
       <h1>The Interface</h1>
-      {dummyAPI.map((nft, index) => {
+      {ombAPI.map((nft, index) => {
         return (
           <div key={index}>
             <h2>{nft.collection.name}</h2>
             <h2>{nft.token.inscriptionNumber}</h2>
             <img src={nft.token.contentURI} />
+            <h4>Timestamp: {nft.createdAt}</h4>
+            <h4>Listed Price: {nft.listedPrice}</h4>
           </div>
         );
       })}
