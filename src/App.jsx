74c083e074e78edd1f617871914218db25b69f4f
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-03-23 14:01:28 -0400

    submission app setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,17 @@
 import "./App.css";
-import ombAPI from "./ombAPI";
-import catsAPI from "./catsAPI";
+import { Route, Routes } from "react-router-dom";
+import TheInterface from "./components/TheInterface.jsx";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <h1>The Interface</h1>
-      <h1>OMB</h1>
-      {ombAPI.map((nft, index) => {
-        return (
-          <div key={index}>
-            <h2>{nft.collection.name}</h2>
-            <h2>{nft.token.inscriptionNumber}</h2>
-            <img src={nft.token.contentURI} />
-            <h4>Timestamp: {nft.createdAt}</h4>
-            <h4>Listed Price: {nft.listedPrice}</h4>
-          </div>
-        );
-      })}
-      <h1>Quantum Cats</h1>
-      {catsAPI.map((nft, index) => {
-        return (
-          <div key={index}>
-            <h2>{nft.meta.name}</h2>
-            <h2>{nft.inscriptionNumber}</h2>
-            {/* unable to render images due to Third-party Cookies */}
-            <img src={nft.contentURI} />
-            <h4>Timestamp: {nft.listedAt}</h4>
-            <h4>Listed Price: {nft.listedPrice}</h4>
-          </div>
-        );
-      })}
+      {/* navbar here */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/thePuzzle" element={<ThePuzzle />} /> */}
+        <Route path="/theInterface" element={<TheInterface />} />
+      </Routes>
     </>
   );
 }
