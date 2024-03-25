5b2cf44e22d4f338e89472bc9421d7f7e289a595
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-03-25 09:10:47 -0400

    javascript solution for theDatabase

--- src/components/theDatabaseVertical.js
+++ src/components/theDatabaseVertical.js
@@ -16,8 +16,33 @@ function returnPattern(patternsTable, hashTable, patternDirection, blockNum) {
         result.push(hash);
       }
     }
+  } else if (patternDirection === "v") {
+    let blockNumber = blockNum.toString();
+    const verticalSplit = Object.keys(patternsTable[1])[0].split("");
+    let arrayToSearch = [];
+    for (const block of hashTable) {
+      if (Object.keys(block)[0] === blockNumber) {
+        arrayToSearch.push(block);
+      }
+    }
+    if (arrayToSearch.length < 2) {
+      return "There are not enough hashes to compare";
+    }
+    for (let i = 0; i < arrayToSearch.length - 1; i++) {
+      let hashToCompare = Object.values(arrayToSearch[i])[0];
+      let nextHash = Object.values(arrayToSearch[i + 1])[0];
+      let nextHashSplit = nextHash.split("");
+      for (let j = 0; j < hashToCompare.length; j++) {
+        const splitHash = hashToCompare.split("");
+        if (splitHash[j] === verticalSplit[0]) {
+          if (nextHashSplit[j] === verticalSplit[1]) {
+            result.push(arrayToSearch[i], arrayToSearch[i + 1]);
+          }
+        }
+      }
+    }
   }
   return result;
 }
 
-console.log(returnPattern(patternsTable, hashTable, "h"));
+console.log(returnPattern(patternsTable, hashTable, "v", 12345));
